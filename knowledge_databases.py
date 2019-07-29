8014b31cd7ae860f7417a8b2104c08ad3f0bd7a1
myname21-meet <myname21@meet.mit.edu>
2019-07-29 14:15:29 +0300

    database

--- knowledge_databases.py
+++ knowledge_databases.py
@@ -8,20 +8,46 @@ Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def add_article():
-	pass
+def add_article(artical_name, topic, rating):
+        artical_object=Knowledge(
+                artical_name=artical_name,
+                topic=topic,
+                rating=rating)
+        session.add(artical_object)
+        session.commit()
+add_article("pizza", "food", 10)
+add_article("burger", "food", 8)
+add_article("fish", "animal", 7)
+add_article("dog", "animal", 5)
+add_article("almog", "name", 10)
+	
 
 def query_all_articles():
-	pass
+	articals=session.query(Knowledge).all()
+	return articals
+print(query_all_articles())	
 
-def query_article_by_topic():
-	pass
+def query_article_by_topic(their_topic):
+	atical=session.query(Knowledge).filter_by(topic=their_topic).first()
+	return atical
+print(query_article_by_topic("food"))
 
-def delete_article_by_topic():
-	pass
+
+def delete_article_by_topic(topic):
+	session.query(Knowledge).filter_by(topic=topic).delete()
+	session.commit()
+delete_article_by_topic("food")
 
 def delete_all_articles():
-	pass
+	session.query(Knowledge).delete()
+delete_all_articles()
 
-def edit_article_rating():
-	pass
+
+	
+
+def edit_article_rating(artical_name,rating):
+	artical_object=session.query(Knowledge).filter_by(artical_name=artical_name).first()
+	artical_object.rating=rating
+	session.commit()
+edit_article_rating("dog", 10)
+	
--- knowledge_model.py
+++ knowledge_model.py
@@ -6,12 +6,17 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class Knowledge(Base):
-	# Create a table with 4 columns
-	# The first column will be the primary key
-	# The second column should be a string representing
-	# the name of the Wiki article that you're referencing
-	# The third column will be a string representing the 
-	# topic of the article. The last column will be
-	# an integer, representing your rating of the article.
-
-	pass
+        __tablename__='articals'
+        artical_id=Column(Integer, primary_key=True)
+        artical_name= Column(String)
+        topic=Column(String)
+        rating=Column(Integer)
+        def __repr__(self):
+                return ("artica name: {}\n"
+                        "artical topic: {}\n"
+                        "rating:{}").format(
+                                self.artical_name,
+                                self.topic,
+                                self.rating)
+a=Knowledge(artical_name="bla", topic="bla bla", rating=10)
+print (a)
